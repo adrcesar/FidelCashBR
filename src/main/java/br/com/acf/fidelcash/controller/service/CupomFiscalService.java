b63f80420204946cd49124037b1df7931eaf7c73
Adriano Ferreira <adrcesar@gmail.com>
2020-01-11 16:16:35 -0300

    mensagem de erro de arquivos xml duplicados

--- src/main/java/br/com/acf/fidelcash/controller/service/CupomFiscalService.java
+++ src/main/java/br/com/acf/fidelcash/controller/service/CupomFiscalService.java
@@ -16,19 +16,20 @@ public class CupomFiscalService {
 	private CupomFiscalRepository cfRespository;
 
 	public CupomFiscal setCupomFiscal(CupomFiscal cupomFiscal, Cliente cliente) throws CupomFiscalServiceException {
-        try {
-            cupomFiscal.setCliente(cliente);
 
-            Optional<CupomFiscal> cupomFiscalFind = cfRespository.findByCodigoCupomAndCliente(cupomFiscal.getCodigoCupom(), cliente);
-            if (cupomFiscalFind.isPresent()) {
-                throw new CupomFiscalServiceException("registro duplicado", "registro duplicado");
-            }
-            cfRespository.save(cupomFiscal);
-            cupomFiscalFind = cfRespository.findByCodigoCupomAndCliente(cupomFiscal.getCodigoCupom(), cliente);
-            return cupomFiscalFind.get();
-        } catch (Exception e) {
-            throw new CupomFiscalServiceException("Erro ao gerar cupom fiscal", "Erro ao gerar cupom fiscal");
-        }
-    }
+		cupomFiscal.setCliente(cliente);
+		Optional<CupomFiscal> cupomFiscalFind = cfRespository.findByCodigoCupomAndCliente(cupomFiscal.getCodigoCupom(),
+				cliente);
+		if (cupomFiscalFind.isPresent()) {
+			throw new CupomFiscalServiceException("registro duplicado", "registro duplicado");
+		}
+		try {
+			cfRespository.save(cupomFiscal);
+			cupomFiscalFind = cfRespository.findByCodigoCupomAndCliente(cupomFiscal.getCodigoCupom(), cliente);
+			return cupomFiscalFind.get();
+		} catch (Exception e) {
+			throw new CupomFiscalServiceException("Erro ao gerar cupom fiscal", "Erro ao gerar cupom fiscal");
+		}
+	}
 
 }
